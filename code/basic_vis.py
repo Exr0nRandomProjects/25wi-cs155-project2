b8a388330aacfc36e690044acaba92d6c25e1ba8
Exr0n <mail@exr0n.com>
2025-02-28 20:25:24 -0800

    more basic vis (+ skew)

--- code/basic_vis.py
+++ code/basic_vis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from scipy.stats import skew
 
 data = pd.read_csv('../data/data.csv')
 movies = pd.read_csv('../data/movies.csv')
@@ -15,11 +16,15 @@ print(movies.columns)
 
 plt.rcParams['savefig.dpi'] = 200
 plt.rcParams['figure.dpi'] = 200
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['axes.labelweight'] = 'bold'
 
 # %%
 # all ratings
 def plot_all_ratings():
-    ax = sns.histplot(data['Rating'], bins=list(range(1, 7)))
+    fig, ax = plt.subplots(figsize=(4, 3))
+    sns.histplot(data['Rating'], bins=list(range(1, 7)), ax=ax)
     ax.set_xlabel('Rating')
     ax.set_ylabel('Count')
     ax.set_xticks(np.arange(1.5, 6.5, 1))
@@ -35,13 +40,15 @@ def plot_most_popular(data):
     most_popular = data.groupby('Movie ID').count() \
         .sort_values('User ID', ascending=False).index[:10]
     data_popular = data[data['Movie ID'].isin(most_popular)]
-    data_popular['Movie ID'] = data_popular['Movie ID'].map(lambda mid: movies[movies['Movie ID'] == mid].iloc[0]['Movie Title'])
-    ax = sns.histplot(data=data_popular, x='Movie ID', hue='Rating', multiple='dodge')
+    data_popular['Movie Name'] = data_popular['Movie ID'].map(lambda mid: movies[movies['Movie ID'] == mid].iloc[0]['Movie Title'])
+    ax = sns.histplot(data=data_popular, x='Movie Name', hue='Rating', multiple='dodge')
     ax.set_title('Most popular')
     # ax.tick_params(axis='x', rotation=60, labelright=0, ha='right')
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha='right', rotation_mode='anchor')
-plot_most_popular(data)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=25, ha='right', rotation_mode='anchor')
+    return most_popular, data_popular
+most_popular, data_popular = plot_most_popular(data)
 plt.show()
+
 
 # %% most popular, show num votes
 
@@ -52,9 +59,10 @@ def plot_highest_rated(data):
     highest_rated = data.groupby('Movie ID').mean() \
         .sort_values('Rating', ascending=False).index[:10]
     data_best = data[data['Movie ID'].isin(highest_rated)]
-    data_best['Movie ID'] = data_best['Movie ID'].map(lambda mid: movies[movies['Movie ID'] == mid].iloc[0]['Movie Title'])
-    ax = sns.histplot(data=data_best, x='Movie ID', hue='Rating', multiple='dodge')
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha='right', rotation_mode='anchor')
+    data_best['Movie Name'] = data_best['Movie ID'].map(lambda mid: movies[movies['Movie ID'] == mid].iloc[0]['Movie Title'])
+    fig, ax = plt.subplots(figsize=(5, 3))
+    sns.histplot(data=data_best, x='Movie Name', hue='Rating', multiple='dodge', ax=ax)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=25, ha='right', rotation_mode='anchor')
     ax.set_title('Highest rated')
 plot_highest_rated(data)
 plt.show()
@@ -63,7 +71,7 @@ plt.show()
 # visualization of genres 
 
 def plot_genres(data, genres):
-    fig, axs = plt.subplots(1, len(genres), figsize=(12, 5), sharey=True, sharex=True)
+    fig, axs = plt.subplots(1, len(genres), figsize=(9, 3), sharey=True, sharex=True)
     fig.suptitle('Rating distribution by Genre')
     for ax, genre in zip(axs, genres):
         movie_ids = movies[movies[genre] > 0]['Movie ID']
@@ -72,6 +80,30 @@ def plot_genres(data, genres):
         # movies_g['Movie ID'] = movies_g['Movie ID'].map(lambda mid: movies[movies['Movie ID'] == mid].iloc[0]['Movie Title'])
         sns.histplot(ax=ax, data=movies_g, x='Rating', bins=np.arange(0.5, 5.6, 1))
         ax.set_xticks(np.arange(1, 5.5, 1))
-        ax.set_xticklabels(list(range(1, 6)))
-        ax.set_title(genre)
+        ax.set_xticklabels(list(range(1, 6)), weight='bold')
+        ax.set_title(genre, weight='bold')
+    plt.tight_layout()
+    plt.show()
 plot_genres(data, ['Adventure', 'Crime', 'Sci-Fi'])
+
+# %% 
+# skew vis 
+
+def plot_skew(data, most_popular):
+    global_skew = skew(data['Rating'])
+    print("skew of all ratings:", global_skew)
+    fig, ax = plt.subplots(figsize=(4, 3))
+    skews = [skew(data[data['Movie ID'] == m]['Rating']) for m in most_popular]
+    print(skews)
+    movie_name_by_id = { k: v for k, v in zip(movies['Movie ID'], movies['Movie Title']) }
+    movie_names = [movie_name_by_id[i] for i in most_popular]
+    print(movie_names, skews)
+    sns.barplot(x=movie_names, y=skews, ax=ax)
+    ax.hlines([global_skew], label='Global Skew', xmin=ax.get_xlim()[0], xmax=ax.get_xlim()[1], color='black', lw=2, ls='--')
+    ax.legend()
+    ax.set_title("Skew of most popular movies")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right', rotation_mode='anchor')
+    ax.set_xlabel('Movie Title')
+    ax.set_ylabel('Skew')
+    plt.show()
+plot_skew(data, most_popular)
